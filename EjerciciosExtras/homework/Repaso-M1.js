53c5a1ae47ece300871261c4dbe31dc4399b4e83
se7enwin <se7enwin@hotmail.com>
2023-01-27 00:02:19 -0300

    Correccion enunciado

--- EjerciciosExtras/homework/Repaso-M1.js
+++ EjerciciosExtras/homework/Repaso-M1.js
@@ -96,7 +96,7 @@ LinkedList.prototype.changeNotNumbers = function () {
 // Ejemplo:
 // - queueOne: [7,3,5]
 // - queueTwo: [2,4,6]
-// mergeQueues(queueOne, queueTwo) --> [7, 2]
+// mergeQueues(queueOne, queueTwo) --> [7, 2,3,4,5,6]
 // IMPORTANTE: NO son arreglos sino que son Queues.
 // RESUELTO
 
